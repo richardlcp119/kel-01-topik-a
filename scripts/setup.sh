67330ce0c679f2d65d7f22027813edbf8c8406e7
ubuntu <ahmadariffatahillah1267@gmail.com>
2026-05-25 07:36:49 +0000

    Initial setup topik A

---
+++ scripts/setup.sh
@@ -0,0 +1,17 @@
+echo "Updating system..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing Docker..."
+sudo apt install docker.io docker-compose -y
+
+echo "Installong Docker..."
+sudo systemctl enable docker
+sudo systemctl start docker
+
+echo "Installing UFW..."
+sudo apt install ufw -y
+
+echo "Installing Fail2Ban..."
+sudo apt install fail2ban -y
+
+echo "Setup Selesai!"
